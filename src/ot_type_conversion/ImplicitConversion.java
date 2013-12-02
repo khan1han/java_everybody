c614d8db4f964924950c6c39179c149554713da7
egoing <egoing@gmail.com>
2013-12-03 01:28:09 +0900

    예제등록

--- src/ot_type_conversion/ImplicitConversion.java
+++ src/ot_type_conversion/ImplicitConversion.java
@@ -3,8 +3,8 @@ package ot_type_conversion;
 public class ImplicitConversion {
 
 	public static void main(String[] args) {
-		float a = 100.0;
-		int b = 100.0F;
+		//float a = 100.0;
+		//int b = 100.0F;
 	}
 
 }
